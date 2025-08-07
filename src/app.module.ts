74cbab21ddb162ba25b6bb678c06482ed4f1c387
igoramf <igoraaf1@gmail.com>
2025-08-07 08:20:57 -0300

    feat: integrate ChargesModule into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ChargesModule } from './charges/charges.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    ChargesModule,
   ],
   controllers: [],
   providers: [],
